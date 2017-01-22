e74fe6d42a9faaa6b455d68ef7a3ba6d5cc8acdc
MANN <MANN@10.250.0.54>
2017-01-22 18:49:20 -0500

    first

---
+++ src/LabWeek2/LabWeek2.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package LabWeek2;
+
+/**
+ *
+ * @author MANN
+ */
+public class LabWeek2 {
+    public static void main(String[] args)
+    {
+    System.out.println("Hello World, my GitHub account name is hsmann07");
+    }
+}
