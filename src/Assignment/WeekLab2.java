9134ae11d68ffa0f4a534fab3946090891adabdc
MANN <MANN@10.250.0.54>
2017-01-22 19:31:27 -0500

    first

---
+++ src/Assignment/WeekLab2.java
@@ -0,0 +1,21 @@
+package Assignment;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author MANN
+ */
+public class WeekLab2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    System.out.println("Hello World, my GitHub account name is hsmann07");
+    }
+    }
